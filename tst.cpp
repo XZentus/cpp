4ab3d06ba3a8c724890be92e0a002480af4b3ecf
XZentus <z_xmn@mail.ru>
2018-03-16 21:26:50 +0300

    Update tst.cpp

--- tst.cpp
+++ tst.cpp
@@ -11,13 +11,21 @@ struct Pos {
 
 struct Line {
     int xmin, xmax, ymin, ymax;
-}
+};
 
 bool pos_in_line(const Pos & p, const Line & l) {
-    return    l.x >= p.xmin
-           && l.x <= p.xmax 
-           && l.y >= p.ymin
-           && l.y <= p.ymax;
+    return    p.x >= l.xmin
+           && p.x <= l.xmax 
+           && p.y >= l.ymin
+           && p.y <= l.ymax;
+}
+
+int get_diff_x(const Line & l) {
+    return l.xmax - l.xmin + 1;
+}
+
+int get_diff_y(const Line & l) {
+    return l.ymax - l.ymin + 1;
 }
 
 int main() {
@@ -26,11 +34,21 @@ int main() {
     Pos q;
     
     scanf("%d%d%d%d", &s, &k, &q.x, &q.y);
-    U_.xmin = U_.xmax = D_.xmin = D_xmax = q.x;
+    U_.xmin = U_.xmax = q.x;
     U_.ymin = q.y + 1;
-    U_.xmax = s;
+    U_.ymax = s;
+    
+    D_.xmin = D_.xmax = q.x;
     D_.ymin = 1;
-    D_.ymax = q - 1;
+    D_.ymax = q.y - 1;
+    
+    _R.ymin = _R.ymax = q.y;
+    _R.xmin = q.x + 1;
+    _R.xmax = s;
+    
+    _L.ymin = _L.ymax = q.y;
+    _L.xmin = 1;
+    _L.xmax = q.x - 1;
     
     int diff = abs(q.x - q.y);
     int s2 = s / 2;
@@ -54,59 +72,76 @@ int main() {
     DR.xmax = (q.y < s2) ? (s - diff) : s;
     DR.ymin = (q.x > s2) ? diff : 1;
     DR.ymax = q.y - 1;
-//----------------------------------------------------------------
+#define fstr(attr) printf( #attr " x [%d : %d] y [%d : %d] || x: %d y: %d\n", attr.xmin, attr.xmax, attr.ymin, attr.ymax, get_diff_x(attr), get_diff_y(attr))
+    
+    fstr(U_);
+    fstr(D_);
+    fstr(_R);
+    fstr(_L);
+    fstr(UL);
+    fstr(UR);
+    fstr(DR);
+    fstr(DL);
+#undef fstr
+    cout << "-----------------------------------------\n";
     Pos p;
     while(k--) {
         scanf("%d%d", &p.x, &p.y);
         //U
-        if      (p.x == q.x && p.y >= q.y && p.y <= U_.y)
-            U_.y = p.y - 1;
+        if      (pos_in_line(p, U_))
+            U_.ymax = p.y - 1;
         //D
-        else if (p.x == q.x && p.y <= q.y && p.y >= D_.y)
-            D_.y = p.y + 1;
+        else if (pos_in_line(p, D_))
+            D_.ymin = p.y + 1;
         //L
-        else if (p.y == q.y && p.x <= q.x && p.x >= _L.y)
-            _L.x = p.x + 1;
+        else if (pos_in_line(p, _L))
+            _L.xmin = p.x + 1;
         //R
-        else if (p.y == q.y && p.x >= q.x && p.x <= _R.y)
-            _R.x = p.x - 1;
+        else if (pos_in_line(p, _R))
+            _R.xmax = p.x - 1;
         //UL
-        else if (p.y >= q.y && p.x <= q.x && p.y - q.y == q.x - p.x && p.y <= UL.y) {
-            UL.x = p.x + 1;
-            UL.y = p.y - 1;
+        else if (pos_in_line(p, UL)) {
+            UL.xmin = p.x + 1;
+            UL.ymax = p.y - 1;
         }
         //UR
-        else if (p.y >= q.y && p.x >= q.x && p.y - q.y == p.x - q.x && p.y <= UR.y) {
-            UR.x = p.x - 1;
-            UR.y = p.y - 1;
+        else if (pos_in_line(p, UR)) {
+            UR.xmax = p.x - 1;
+            UR.ymax = p.y - 1;
         }
         //DL
-        else if (p.y <= q.y && p.x <= q.x && q.y - p.y == q.x - p.x && p.y >= DL.y) {
-            DL.x = p.x + 1;
-            DL.y = p.y + 1;
+        else if (pos_in_line(p, DL)) {
+            DL.xmin = p.x + 1;
+            DL.ymin = p.y + 1;
         }
         //DR
-        else if (p.y <= q.y && p.x >= q.x && q.y - p.y == p.x - q.x && p.y >= DR.y) {
-            DR.x = p.x - 1;
-            DR.y = p.y + 1;
+        else if (pos_in_line(p, DR)) {
+            DR.xmax = p.x - 1;
+            DR.ymin = p.y + 1;
         }
     }
-/*    
-    cout << "U_ " << U_.x << ' ' << U_.y << '\n'
-         << "D_ " << D_.x << ' ' << D_.y << '\n'
-         << "_R " << _R.x << ' ' << _R.y << '\n'
-         << "_L " << _L.x << ' ' << _L.y << '\n'
-         << "DR " << DR.x << ' ' << DR.y << '\n'
-         << "UR " << UR.x << ' ' << UR.y << '\n'
-         << "DL " << DL.x << ' ' << DL.y << '\n'
-         << "UL " << UL.x << ' ' << UL.y << '\n' ;
-*/        
+#define fstr(attr) printf( #attr " x [%d : %d] y [%d : %d] || x: %d y: %d\n", attr.xmin, attr.xmax, attr.ymin, attr.ymax, get_diff_x(attr), get_diff_y(attr))
     
-    printf("%d\n", U_.y - D_.y
-                 + _R.x - _L.x
-                 + UR.y - DL.y
-                 + UL.y - DR.y);
-    
+    fstr(U_);
+    fstr(D_);
+    fstr(_R);
+    fstr(_L);
+    fstr(UL);
+    fstr(UR);
+    fstr(DR);
+    fstr(DL);
+#undef fstr
 
+    int u = get_diff_y(U_),
+        d = get_diff_y(D_),
+        l = get_diff_x(_L),
+        r = get_diff_x(_R),
+        ul = get_diff_y(UL),
+        ur = get_diff_y(UR),
+        dl = get_diff_y(DL),
+        dr = get_diff_y(DR);
+#define ps(i) ((i > 0) ? i : 0)
+    printf("%d\n", ps(u) + ps(d) + ps(l) + ps(r) + ps(ul) + ps(ur) + ps(dl) + ps(dr));
+#undef ps
     return 0;
 }
