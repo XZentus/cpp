6695435ae8bec37a0ce593808a01e0be853e72df
XZentus <z_xmn@mail.ru>
2018-04-14 21:09:50 +0300

    Update aoc_2017_5.cpp

--- aoc_2017_5.cpp
+++ aoc_2017_5.cpp
@@ -17,7 +17,23 @@ int task_1() {
     return counter;
 }
 
+int task_2() {
+    int pos = 0, counter = 0, prev_pos, p_val;
+    vector<int> stack;
+    int tmp;
+    while(cin >> tmp)
+        stack.push_back(tmp);
+    while(pos >= 0 && pos < stack.size()) {
+        counter += 1;
+        prev_pos = pos;
+        p_val = stack[pos];
+        pos += p_val;
+        stack[prev_pos] += (p_val >= 3) ? -1 : 1;
+    }
+    return counter;
+}
+
 int main() {
-    cout << task_1() << endl;
+    cout << task_2() << endl;
     return 0;
 }
