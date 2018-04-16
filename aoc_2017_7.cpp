e5f40d5f29ceca94fb6be1a877fcd3753bdaf0ee
XZentus <z_xmn@mail.ru>
2018-04-16 11:57:21 +0300

    Update aoc_2017_7.cpp

--- aoc_2017_7.cpp
+++ aoc_2017_7.cpp
@@ -11,6 +11,7 @@ struct Node {
     int value;
     int total_sum;
     set<string> leaf;
+    Node(): total_sum(0) {};
 };
 
 int parse(const string & v) {
@@ -48,8 +49,73 @@ void task_1() {
     return;
 }
 
+void fill_sums(map<string, Node> & db, const string & root) {
+    auto & current = db[root];
+    if(current.total_sum == 0){
+        if(current.leaf.empty())
+            return;
+        for(auto & leaf_name: current.leaf) {
+            fill_sums(db, leaf_name);
+            current.total_sum += db[leaf_name].total_sum + db[leaf_name].value;
+        }
+    }
+    return;
+}
+
+void print_level(map<string, Node> & db, const string & root) {
+    cout << root << ": " << db[root].value << " + " << db[root].total_sum << " = " << db[root].value + db[root].total_sum << endl;
+    for(const auto & s: db[root].leaf)
+        cout << "    " << s << ": " << db[s].value << " + " << db[s].total_sum << " = " << db[s].value + db[s].total_sum << endl;
+}
+
+bool check_leaves(map<string, Node> & db, const string & root) {
+	if (db[root].leaf.empty())
+		return true;
+
+    auto iter = db[root].leaf.begin();
+    int val1 = db[*iter].value + db[*iter].total_sum;
+	for(auto end = db[root].leaf.end(); iter != end; ++iter) {
+		if(!check_leaves(db, *iter))
+			return false;
+        if(val1 != db[*iter].value + db[*iter].total_sum) {
+            print_level(db, root);
+			//return false;
+		}
+	}
+	return true;
+}
+
+void task_2() {
+    map<string, Node> db;
+    
+    string line, word, node_name, val_str;
+    while(getline(cin, line) && line.size()) {
+        istringstream inp(line);
+        inp >> node_name;
+        inp >> val_str;
+        if(inp >> word) { // "->"
+            while(inp >> word && word.size()) {
+                if(word[word.size() - 1] == ',')
+                    word.pop_back();
+                db[word].root = word;
+                db[node_name].leaf.insert(word);
+            }
+        }
+        db[node_name].value = parse(val_str);
+    }
+    
+    string root;
+    for(auto citer = db.cbegin(); citer != db.cend(); ++citer)
+        if(citer->second.root == "")
+            root = citer->first;
+    
+	fill_sums(db, root);
+	check_leaves(db, root);
+    return;
+}
+
 int main() {
-    task_1();
+    task_2();
     
     return 0;
 }
