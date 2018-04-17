18c15094e3817bf8be6353905e1298543abe7c5a
XZentus <z_xmn@mail.ru>
2018-04-17 10:06:17 +0300

    Create aoc_2017_9.cpp

---
+++ aoc_2017_9.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+const char * parse_garbage(const char * str) {
+    while(*str && *str != '>') {
+        if(*str == '!')
+            ++str;
+		++str;
+    }
+	return str;
+}
+
+const char * parse_group(const char * str, int & sum, int n) {
+	sum += n;
+	while(*str && *str != '}') {
+        if(*str == '{') {
+            ++str;
+            str = parse_group(str, sum, n + 1);
+        }
+        else if(*str == '<') {
+            ++str;
+            str = parse_garbage(str);
+        }
+		++str;
+	}
+	return str;
+}
+
+int parse(const char * str) {
+    int sum = 0;
+    while(*str) {
+        if(*str == '{') {
+            ++str;
+            str = parse_group(str, sum, 1);
+        }
+        else if(*str == '<') {
+            ++str;
+            str = parse_garbage(str);
+        }
+		++str;
+    }
+
+	return sum;
+}
+
+int main() {
+	
+	string input;
+	cin >> input;
+	
+	cout << parse(input.c_str()) << endl;
+	
+	return 0;
+}
