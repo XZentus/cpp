d7212cad5aa4f6e63afb0198310d8dfcd284ecfe
XZentus <z_xmn@mail.ru>
2018-04-17 13:58:35 +0300

    Update aoc_2017_10.cpp

--- aoc_2017_10.cpp
+++ aoc_2017_10.cpp
@@ -3,43 +3,42 @@
 
 using namespace std;
 
-const int N = 256;
+const int N = 5;
 
 void print_arr(int * array) {
-	for(int i = 0; i < N; i += 1)
-		cout << array[i] << ' ';
-	cout << endl;
+    for(int i = 0; i < N; i += 1)
+        cout << array[i] << ' ';
+    cout << endl;
 }
 
 void task_1() {
-	int array[N];
-	for(int i = 0; i < N; i += 1)
-		array[i] = i;
-	
-	int current_position = 0,
-	    skip_size        = 0,
-		length;
+    int array[N];
+    for(int i = 0; i < N; i += 1)
+        array[i] = i;
+    
+    int current_position = 0,
+        skip_size        = 0,
+        length;
     char c;
-	while(cin >> length) {
-		cin >> c;
-		
-		for(int i = 0; i < length / 2; i += 1) {
-			int tmp = array[(current_position + i) % N];
-			array[(current_position + i) % N] = array[(current_position + i + length - 1) % N];
-			array[(current_position + i + length - 1) % N] = tmp;
-		}
-		print_arr(array);
-		current_position += (length + skip_size) % N;
-		skip_size += 1;
-			
-	}
-    cout << array[0] * array[1] << endl;
-	return;
+    while(cin >> length) {
+        cin >> c;
+        
+        for(int i = 0; i < length / 2; i += 1) {
+            int tmp = array[i % N];
+            array[i % N] = array[(i + length) % N];
+            array[(i + length) % N] = tmp;
+        }
+        print_arr(array);
+        current_position += length + skip_size;
+        skip_size += 1;
+            
+    }
+    return;
 }
 
 int main() {
     
-	task_1();
-	
+    task_1();
+    
     return 0;
 }
