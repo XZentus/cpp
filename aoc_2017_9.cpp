4dd00d501008cc8b85be8a3ab7696e7dd538b06c
XZentus <z_xmn@mail.ru>
2018-04-17 10:11:32 +0300

    Update aoc_2017_9.cpp

--- aoc_2017_9.cpp
+++ aoc_2017_9.cpp
@@ -3,18 +3,23 @@
 
 using namespace std;
 
+static int garbage_count = 0;
+
 const char * parse_garbage(const char * str) {
     while(*str && *str != '>') {
-        if(*str == '!')
-            ++str;
-		++str;
+        if(*str == '!') {
+            str += 2;
+            continue;
+        }
+        ++garbage_count;
+        ++str;
     }
-	return str;
+    return str;
 }
 
 const char * parse_group(const char * str, int & sum, int n) {
-	sum += n;
-	while(*str && *str != '}') {
+    sum += n;
+    while(*str && *str != '}') {
         if(*str == '{') {
             ++str;
             str = parse_group(str, sum, n + 1);
@@ -23,9 +28,9 @@ const char * parse_group(const char * str, int & sum, int n) {
             ++str;
             str = parse_garbage(str);
         }
-		++str;
-	}
-	return str;
+        ++str;
+    }
+    return str;
 }
 
 int parse(const char * str) {
@@ -39,18 +44,20 @@ int parse(const char * str) {
             ++str;
             str = parse_garbage(str);
         }
-		++str;
+        ++str;
     }
 
-	return sum;
+    return sum;
 }
 
 int main() {
-	
-	string input;
-	cin >> input;
-	
-	cout << parse(input.c_str()) << endl;
-	
-	return 0;
+    
+    string input;
+    cin >> input;
+    garbage_count = 0;
+    int sum = parse(input.c_str());
+    
+    cout << "Groups sum: " << sum << '\n' << "Garbage deleted: " << garbage_count << endl;
+    
+    return 0;
 }
