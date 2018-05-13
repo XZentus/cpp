caf7a9dda1f60a0831058163ae62111feecdb408
XZentus <z_xmn@mail.ru>
2018-05-13 22:42:02 +0300

    Create eval.cpp

---
+++ eval.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+struct expr {
+    virtual double operator()(double) = 0;
+};
+
+struct const_value : public expr {
+    double _value;
+    const_value (double value = 0): _value(value) {};
+    double operator()(double) { return _value; };
+};
+
+struct argument : public expr {
+    double operator()(double x = 0) { return x; };
+};
+
+#define make_bin_op(_op_name, _op) struct _op_name : public expr {         \
+    expr _left;                                                            \
+    expr _right;                                                           \
+    _op_name(expr left, expr right): _left(left), _right(right) {};        \
+    double operator()(double arg) { return _left(arg) _op _right(arg); };  \
+}
+
+make_bin_op(add,  +);
+make_bin_op(sub,  -);
+make_bin_op(prod, *);
+make_bin_op(div,  /);
+
+#undef make_bin_op
+
+int main()
+{
+    std::cout << "Hello, world!\n";
+}
