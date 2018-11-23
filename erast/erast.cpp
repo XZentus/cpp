6ec536d59e9e74a301515f2a99d0b24286d75b4d
XZentus <z_xmn@mail.ru>
2018-11-23 13:28:37 +0300

    Create erast.cpp

---
+++ erast/erast.cpp
@@ -0,0 +1,91 @@
+#include <ios>
+#include <iomanip>
+#include <iostream>
+
+#include <cstdlib>
+#include <cstring>
+
+using namespace std;
+
+const size_t LIMIT        = 1000,
+             BITS_IN_BYTE = 8,
+             DB_SIZE      = LIMIT/BITS_IN_BYTE + 1;
+
+unsigned char erast[DB_SIZE] = { 0 };
+
+void drop_bit(size_t i) {
+    erast[i / BITS_IN_BYTE] &= ~(0x01 << (i % BITS_IN_BYTE));
+}
+
+bool is_prime(const size_t & i) {
+    return erast[i / BITS_IN_BYTE] & (0x01 << (i % BITS_IN_BYTE));
+}
+
+size_t next_prime(size_t i) {
+    i += 1;
+    while( !is_prime(i))
+        i += 1;
+
+    return i;
+}
+
+void init() {
+    memset(erast, 0xff, DB_SIZE);
+    
+    for(size_t i = 4; i < LIMIT; i += 2)
+        drop_bit(i);
+    
+    for(size_t i = 3; i * i < LIMIT; i = next_prime(i))
+        for(size_t n = i * i; n < LIMIT; n += i)
+            drop_bit(n);
+}
+
+void print_erast_bits() {
+    for(size_t i = 0; i < LIMIT; i += 1)
+        cout << is_prime(i);
+    cout << endl;
+}
+
+void print_erast_hex_simple(const size_t & row_len) {
+    cout << hex << uppercase << setfill('0');
+    for(size_t i = 0; i < DB_SIZE - 1; i += 1) {
+        cout << "0x" << setw(2) << (erast[i] & 0xff) << ", ";
+        if ( (i % row_len) == (row_len - 1) )
+            cout << '\n';
+    }
+    cout << "0x" << (erast[DB_SIZE - 1] & 0xff);
+    
+    cout << dec << nouppercase << setfill(' ');
+}
+
+void print_erast_dec_simple(const size_t & row_len) {
+    const int w = 3;
+    for(size_t i = 0; i < DB_SIZE - 1; i += 1) {
+        cout << setw(w) << (erast[i] & 0xff) << ", ";
+        if ( (i % row_len) == (row_len - 1) )
+            cout << '\n';
+    }
+    cout << setw(w) << (erast[DB_SIZE - 1] & 0xff);
+    
+}
+
+int main()
+{
+    cout << "DB_SIZE = " << DB_SIZE << '\n'
+         << "LIMIT   = " << LIMIT << '\n';
+    init();
+    
+    print_erast_hex_simple(16);
+    
+    cout << "\n\n";
+    
+    print_erast_dec_simple(16);
+    
+    cout << "\n\n";
+    /*
+    for(size_t i = 2; i < LIMIT; i = next_prime(i))
+        if(is_prime(i))
+            cout << i << ' ';
+    */
+    return 0;
+}
