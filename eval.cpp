fa46c0681b72f6a0789af85d5573d76cd3715ce7
XZentus <z_xmn@mail.ru>
2019-01-20 23:14:33 +0300

    Update eval.cpp

--- eval.cpp
+++ eval.cpp
@@ -1,60 +1,77 @@
 #include <iostream>
+#include <memory>
 #include <cmath>
 
 using namespace std;
 
-struct expr {
-    virtual double operator()(double) {
-        cerr << "EXPR::operator() called\n";
-        return 100500;
-    };
+const double FITNESS_MIN          =   -7.0;
+const double FITNESS_MAX          =    7.0;
+const int    FITNESS_POINTS       = 2000;
+const double FITNESS_STEP         = (FITNESS_MAX - FITNESS_MIN) / double(FITNESS_POINTS - 1);
+
+const double FUNCTION_PROBABILITY =    0.5;
+const double ARG_PROBABILITY      =    0.7;
+const double MIN_VAL              =  -20.0;
+const double MAX_VAL              =   20.0;
+
+const double MUTATE_ARG           =    0.1;
+const double MUTATE_NUM           =    0.6;
+const double MUTATE_FUN           =    0.2;
+const double MUTATE_MIN           =   -2.0;
+const double MUTATE_MAX           =    2.0;
+
+const size_t POPULATION_SIZE      =   80;
+const size_t INDIVIDUALS_SURVIVE  =   30;
+const double CHANCE_DUPLICATE     =    0.02;
+
+const double EXCEPTION_WEIGHT     = 10000.0;
+
+const size_t DEPTH                = 10;
+
+class Expr {
+    enum etype { Add, Sub, Mul, Div, Sin, Cos, Tan, Num, Arg };
+    
+    etype type;
+    
+    unique_ptr<Expr> left;
+    unique_ptr<Expr> right;
+    double n;
+    
+    Expr(const size_t &);
+    
+    double operator()(const double &);
+    
+    void simplify(const size_t &);
 };
 
-struct const_value : public expr {
-    double _value;
-    const_value (double value = 0): _value(value) {};
-    double operator()(double arg = 0) override {
-        cout << "const_value " << _value << " called arg = " << arg << '\n';
-        return _value;
-    };
-};
-
-#define make_bin_op(_op_name, _op) struct e_##_op_name : public expr {     \
-    expr _left;                                                            \
-    expr _right;                                                           \
-    e_##_op_name(expr left, expr right): _left(left), _right(right) {};    \
-    double operator()(double arg) override {                               \
-        return _left(arg) _op _right(arg);                                 \
-    };                                                                     \
+double Expr::operator()(const double & x) {
+    double left = 0, right = 0;
+    if(this->type < Num)
+        left = this->left->operator()(x);
+    if(this->type < Sin)
+        right = this->right->operator()(x);
+    switch(this->type) {
+        case Add:
+            return left + right;
+        case Sub:
+            return left - right;
+        case Mul:
+            return left * right;
+        case Div:
+            return left / right;
+        case Sin:
+            return sin(left);
+        case Cos:
+            return cos(left);
+        case Tan:
+            return tan(left);
+        case Num:
+            return this->n;
+    }
+    return x;
 }
-
-make_bin_op(add,  +);
-make_bin_op(sub,  -);
-make_bin_op(prod, *);
-make_bin_op(div,  /);
-
-#undef make_bin_op
-
-#define make_unary_op(_op)  struct e_##_op : public expr {                 \
-    expr _expr;                                                            \
-    e_##_op(expr val): _expr(val) {};                                      \
-    double operator()(double arg) override {                               \
-        cout << #_op " called arg = " << arg << '\n';                      \
-        return _op(_expr(arg)) ; };                                        \
-}
-
-make_unary_op(sin);
-make_unary_op(cos);
-
-#undef make_unary_op
-
-int main()
-{
-    const_value c1{1};
-    e_add e{c1, c1};
     
-    cout << e(100) << endl;
-    cout << c1(100) << endl;
-    
+
+int main() {
     return 0;
 }
